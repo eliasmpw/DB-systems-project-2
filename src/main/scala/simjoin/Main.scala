35badd4f1f12e1d95f2cb9ab20311087a3c686c1
Joachim Desroches <joachim.desroches@epfl.ch>
2019-05-13 18:36:41 +0200

    Works without outer partitions.

--- src/main/scala/simjoin/Main.scala
+++ src/main/scala/simjoin/Main.scala
@@ -12,47 +12,80 @@ import scala.io.Source
 import java.io._
 
 object Main {
-  def main(args: Array[String]) {     
-    val inputFile="../dblp_small.csv"    
+  def main(args: Array[String]) {
+    val inputFile="../dblp_small.csv"
     val numAnchors = 4
     val distanceThreshold = 2
-    val attrIndex = 0    
-        
-    val input = new File(getClass.getResource(inputFile).getFile).getPath    
+    val attrIndex = 0
+
+    val input = new File(getClass.getResource(inputFile).getFile).getPath
     val sparkConf = new SparkConf().setAppName("CS422-Project2").setMaster("local[*]")
     val ctx = new SparkContext(sparkConf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(ctx)   
-    
+    val sqlContext = new org.apache.spark.sql.SQLContext(ctx)
+
     val df = sqlContext.read
     .format("com.databricks.spark.csv")
-    .option("header", "true")
+    .option("header", "false")
     .option("inferSchema", "true")
     .option("delimiter", ",")
-    .load(input)      
-    
-    val rdd = df.rdd        
-    val schema = df.schema.toList.map(x => x.name)    
-    val dataset = new Dataset(rdd, schema)           
-    
-    
-    val t1 = System.nanoTime    
+    .load(input)
+
+    val rdd = df.rdd
+    val schema = df.schema.toList.map(x => x.name)
+    val dataset = new Dataset(rdd, schema)
+
+
+    val t1 = System.nanoTime
     val sj = new SimilarityJoin(numAnchors, distanceThreshold)
-    val res = sj.similarity_join(dataset, attrIndex)           
-    
+    val res = sj.similarity_join(dataset, attrIndex)
+
     val resultSize = res.count
     println(resultSize)
     val t2 = System.nanoTime
-            
+
     println((t2-t1)/(Math.pow(10,9)))
-    
+
     /*
+    // Compute the Levenshtein distance between two strings.
+    def edit_distance(k: String, v: String): Int = {
+      val m = k.size
+      val n = v.size
+      var distanceMatrix: Array[Array[Int]] = Array.ofDim(m + 1, n + 1)
+
+      def min3(a: Int, b: Int, c: Int): Int = { if (a < b) { if (a < c) (a) else c } else { if (b < c) b else c } }
+
+      for(i <- 0 to m) {
+        distanceMatrix(i)(0) = i
+      }
+      for(j <- 0 to n) {
+        distanceMatrix(0)(j) = j
+      }
+
+      for(j <- 1 to n) {
+        for (i <- 1 to m) {
+          if (k(i-1) == v(j-1)) {
+            distanceMatrix(i)(j) = distanceMatrix(i-1)(j-1)
+          } else {
+            distanceMatrix(i)(j) = min3(distanceMatrix(i-1)(j) + 1,   // deletion
+                                        distanceMatrix(i)(j-1) + 1,   // insertion
+                                        distanceMatrix(i-1)(j-1) + 1) // substitution
+          }
+        }
+      }
+      distanceMatrix(m)(n)
+    }
+
     // cartesian
     val t1Cartesian = System.nanoTime
     val cartesian = rdd.map(x => (x(attrIndex), x)).cartesian(rdd.map(x => (x(attrIndex), x)))
-                                   .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString() && edit_distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
-                                   
+                                   .filter(x => (x._1._2(attrIndex).toString() != x._2._2(attrIndex).toString()
+                                     && edit_distance(x._1._2(attrIndex).toString(), x._2._2(attrIndex).toString()) <= distanceThreshold))
+
     println(cartesian.count)
     val t2Cartesian = System.nanoTime
-    println((t2Cartesian-t1Cartesian)/(Math.pow(10,9)))*/    
-  }     
+    println((t2Cartesian-t1Cartesian)/(Math.pow(10,9)))
+
+    */
+
+  }
 }
--- src/main/scala/simjoin/SimilarityJoin.scala
+++ src/main/scala/simjoin/SimilarityJoin.scala
@@ -9,16 +9,77 @@ import org.slf4j.LoggerFactory
 
 class SimilarityJoin(numAnchors: Int, distThreshold:Int) extends java.io.Serializable {
   val logger = LoggerFactory.getLogger("SimilarityJoin")
-  var rdd: RDD[String] = null
-  
+
   /*
    * this method gets as input a dataset and the index of an attribute
    * of the dataset, and returns the result of the similarity self join on
    * that attribute.
-   * */  
+   * */
   def similarity_join(dataset: Dataset, attrIndex: Int) : RDD[(String, String)] = {
-    
-    null
+
+    val rdd: RDD[String] = dataset.getRDD().map(_.getString(attrIndex))
+
+    // Should we use `sample` wich returns an RDD ?
+    val anchors: Array[String] = rdd.takeSample(false, numAnchors)
+
+    def compareAll(part: Iterator[(String, String)]): Iterator[(String, String)] = {
+      // FIXME: Ensure that comparisons are done only once.
+      val strings = part.map(_._2).toIndexedSeq
+      val ret = for {
+        i <- 0 to (strings.size - 1)
+        j <- i + 1 to (strings.size - 1)
+        if levenshtein(strings(i), strings(j)) <= distThreshold
+      } yield (strings(i), strings(j))
+      ret.toIterator
+    }
+
+    rdd.flatMap(assign(anchors))
+      .partitionBy(new SimilarityJoinPartitioner(anchors))
+      .mapPartitions(compareAll, true)
+
   }
+
+  // Assign each element of the RDD to it's anchor point
+  def assign(anchors: Array[String])(v: String): List[(String, String)] = {
+    val dists = anchors.map(k => levenshtein(k, v))
+    List((anchors(dists.indices.minBy(dists)), v))
+  }
+
+  // Compute the Levenshtein distance between two strings.
+  def levenshtein(k: String, v: String): Int = {
+    val m = k.size
+    val n = v.size
+    var distanceMatrix: Array[Array[Int]] = Array.ofDim(m + 1, n + 1)
+
+    def min3(a: Int, b: Int, c: Int): Int = { if (a < b) { if (a < c) (a) else c } else { if (b < c) b else c } }
+
+    for(i <- 0 to m) {
+      distanceMatrix(i)(0) = i
+    }
+    for(j <- 0 to n) {
+      distanceMatrix(0)(j) = j
+    }
+
+    for(j <- 1 to n) {
+      for (i <- 1 to m) {
+        if (k(i-1) == v(j-1)) {
+          distanceMatrix(i)(j) = distanceMatrix(i-1)(j-1)
+        } else {
+          distanceMatrix(i)(j) = min3(distanceMatrix(i-1)(j) + 1,   // deletion
+                                      distanceMatrix(i)(j-1) + 1,   // insertion
+                                      distanceMatrix(i-1)(j-1) + 1) // substitution
+        }
+      }
+    }
+    distanceMatrix(m)(n)
+  }
+
 }
 
+class SimilarityJoinPartitioner(anchors: Array[String]) extends org.apache.spark.Partitioner {
+
+  def getPartition(key: Any): Int = anchors.indexOf(key)
+
+  def numPartitions(): Int = anchors.size
+
+}

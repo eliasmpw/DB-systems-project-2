45e088a5ceeaf05c872873212afd1d5d460354c0
eliasmpw <eliasmpw@hotmail.com>
2019-05-19 01:08:31 +0200

    add sampler and some executors

--- src/main/scala/sampling/Executor.scala
+++ src/main/scala/sampling/Executor.scala
@@ -1,43 +1,173 @@
 package sampling
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import java.util.Random
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object Executor {
+
+  def createLineItemDataFrame(desc: Description, session: SparkSession, sampleListPref: List[Int]): DataFrame = {
+    var rddResult: RDD[_] = null
+    val sampleList: List[List[Int]] = desc.sampleDescription.asInstanceOf[List[List[Int]]]
+
+    if (sampleList.isEmpty) {
+      rddResult = desc.samples(0)
+    } else if (sampleList.contains(sampleListPref)) {
+      rddResult = desc.samples(sampleList.indexOf(sampleListPref))
+    } else { // Else pick randomly
+      val randomIndex = new Random(System.currentTimeMillis()).nextInt(sampleList.length)
+      rddResult = desc.samples(randomIndex)
+    }
+
+    session.createDataFrame(rddResult.map(x => Row(x)).map(x => x.get(0).asInstanceOf[Row]), desc.lineitem.schema)
+  }
+
   def execute_Q1(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
     assert(params.size == 1)
-    // For example, for Q1, params(0) is the interval from the where close
+    val interval: String = params(0).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(8, 9, 10)).createOrReplaceTempView("lineitem")
+
+    session.sql(
+      "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty , sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1 - l_discount)) as sum_disc_price, sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order " +
+        "from lineitem " +
+        "where l_shipdate <= date_sub(date('1998-12-01'), " + interval + ") " +
+        "group by l_returnflag, l_linestatus " +
+        "order by l_returnflag, l_linestatus"
+    ).show()
   }
 
   def execute_Q3(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
     assert(params.size == 2)
-    // https://github.com/electrum/tpch-dbgen/blob/master/queries/3.sql
-    // using:
-    // params(0) as :1
-    // params(1) as :2
+    val mktSeg: String = params(0).asInstanceOf[String]
+    val date: String = params(1).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(8, 9, 10)).createOrReplaceTempView("lineitem")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.customer.createOrReplaceTempView("customer")
+
+    session.sql(
+      "select l_orderkey, sum(l_extendedprice * (1 - l_discount)) as revenue, o_orderdate, o_shippriority " +
+        "from customer, orders, lineitem " +
+        "where c_mktsegment = '" + mktSeg + "' and c_custkey = o_custkey and l_orderkey = o_orderkey and o_orderdate < date('" + date + "') " +
+        "and l_shipdate > date('" + date + "') " +
+        "group by l_orderkey, o_orderdate, o_shippriority order by revenue desc, o_orderdate"
+    ).show()
   }
 
   def execute_Q5(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 2)
+    val name: String = params(0).asInstanceOf[String]
+    val date: String = params(1).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(1, 2, 3, 4, 5, 6)).createOrReplaceTempView("lineitem")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.customer.createOrReplaceTempView("customer")
+    desc.supplier.createOrReplaceTempView("supplier")
+    desc.nation.createOrReplaceTempView("nation")
+    desc.region.createOrReplaceTempView("region")
+
+    session.sql(
+      "select n_name, sum(l_extendedprice * (1 - l_discount)) as revenue " +
+        "from customer, orders, lineitem, supplier, nation, region " +
+        "where c_custkey = o_custkey and l_orderkey = o_orderkey and l_suppkey = s_suppkey and c_nationkey = s_nationkey and s_nationkey = n_nationkey and n_regionkey = r_regionkey " +
+        "and r_name = '" + name + "' and o_orderdate >= date('" + date + "')and o_orderdate < add_months(date('" + date + "'), 12) " +
+        "group by n_name order by revenue desc"
+    ).show()
   }
 
   def execute_Q6(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 3)
+    val date: String = params(0).asInstanceOf[String]
+    val discount: String = params(1).asInstanceOf[String]
+    val quantity: String = params(2).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(4, 6, 10)).createOrReplaceTempView("lineitem")
+
+    session.sql(
+      "select sum(l_extendedprice * l_discount) as revenue " +
+        "from lineitem " +
+        "where l_shipdate >= date('" + date + "')  " +
+        "and l_shipdate < add_months(date('" + date + "'),12) " +
+        "and l_discount between " + discount + " - 0.01 and " + discount + " + 0.01 " +
+        "and l_quantity < " + quantity
+    ).show()
   }
 
   def execute_Q7(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 2)
+    val name1: String = params(0).asInstanceOf[String]
+    val name2: String = params(1).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(8, 9, 10)).createOrReplaceTempView("lineitem")
+    desc.supplier.createOrReplaceTempView("supplier")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.customer.createOrReplaceTempView("customer")
+    desc.nation.createOrReplaceTempView("nation")
+
+    session.sql(
+      "select n1.n_name as supp_nation, n2.n_name as cust_nation, year(l_shipdate) as l_year, l_extendedprice * (1 - l_discount) as volume " +
+        "from supplier, lineitem, orders, customer, nation n1, nation n2 " +
+        "where s_suppkey = l_suppkey and o_orderkey = l_orderkey and c_custkey = o_custkey and s_nationkey = n1.n_nationkey and c_nationkey = n2.n_nationkey " +
+        "and ( (n1.n_name = '" + name1 + "' " +
+        "and n2.n_name = '" + name2 + "') " +
+        "or (n1.n_name = '" + name2 + "' " +
+        "and n2.n_name = '" + name1 + "') ) " +
+        "and l_shipdate between date('1995-01-01') and date('1996-12-31')"
+    ).createOrReplaceTempView("shipping")
+
+    session.sql(
+      "select supp_nation, cust_nation, l_year, sum(volume) as revenue " +
+        "from shipping " +
+        "group by supp_nation, cust_nation, l_year " +
+        "order by supp_nation, cust_nation, l_year "
+    ).show()
   }
 
   def execute_Q9(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 1)
+    val likeValue: String = params(0).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(2, 4)).createOrReplaceTempView("lineitem")
+    desc.part.createOrReplaceTempView("part")
+    desc.supplier.createOrReplaceTempView("supplier")
+    desc.partsupp.createOrReplaceTempView("partsupp")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.nation.createOrReplaceTempView("nation")
+
+    session.sql(
+      "select n_name as nation, year(o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount " +
+        "from part, supplier, lineitem, partsupp, orders, nation " +
+        "where s_suppkey = l_suppkey and ps_suppkey = l_suppkey and ps_partkey = l_partkey and p_partkey = l_partkey" +
+        " and o_orderkey = l_orderkey and s_nationkey = n_nationkey and p_name like '%" + likeValue + "%'"
+    ).createOrReplaceTempView("profit")
+
+    session.sql(
+      "select nation, o_year, sum(amount) as sum_profit " +
+        "from profit " +
+        "group by nation, o_year " +
+        "order by nation, o_year desc"
+    ).show()
   }
 
   def execute_Q10(desc: Description, session: SparkSession, params: List[Any]) = {
-    // TODO: implement
+    assert(params.size == 1)
+    val date: String = params(0).asInstanceOf[String]
+
+    createLineItemDataFrame(desc, session, List(8, 9, 10)).createOrReplaceTempView("lineitem")
+    desc.customer.createOrReplaceTempView("customer")
+    desc.orders.createOrReplaceTempView("orders")
+    desc.nation.createOrReplaceTempView("nation")
+
+    session.sql(
+      "select c_custkey, c_name, sum(l_extendedprice * (1 - l_discount)) as revenue, c_acctbal, n_name, c_address, c_phone, c_comment " +
+        "from customer, orders, lineitem, nation " +
+        "where c_custkey = o_custkey and l_orderkey = o_orderkey and o_orderdate >= date('" + date + "') " +
+        "and o_orderdate < add_months(date('" + date + "'), 3) and l_returnflag = 'R' and c_nationkey = n_nationkey " +
+        "group by c_custkey, c_name, c_acctbal, c_phone, n_name, c_address, c_comment " +
+        "order by revenue desc"
+    ).show()
   }
 
   def execute_Q11(desc: Description, session: SparkSession, params: List[Any]) = {
--- src/main/scala/sampling/Sampler.scala
+++ src/main/scala/sampling/Sampler.scala
@@ -1,12 +1,167 @@
 package sampling
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.util.SizeEstimator
 
 object Sampler {
   def sample(lineitem: DataFrame, storageBudgetBytes: Long, e: Double, ci: Double): (List[RDD[_]], _) = {
     lineitem.show
-    lineitem.groupBy()
-    null
+
+    val rows = lineitem.rdd
+    val n = rows.count()
+
+    val priceIndex = 5
+    var z: Double = 2.8
+    if (ci <= 0.99) {
+      z = 2.58
+    } else if (ci <= 0.98) {
+      z = 2.33
+    } else if (ci <= 0.95) {
+      z = 1.96
+    } else if (ci <= 0.9) {
+      z = 1.64
+    } else if (ci <= 0.8) {
+      z = 1.28
+    }
+
+    val samplesList: List[List[Int]] = List(List(4, 6, 10), List(8, 9, 10), List(10, 11, 12, 14), List(4, 13, 14), List(2, 4))
+    lazy val nhSh1 = rows.groupBy(x => (x(4), x(6), x(10)))
+      .map(x => (x._1, x._2.size, x._2.map(y => y(priceIndex).asInstanceOf[java.math.BigDecimal].doubleValue()).toList))
+      .map(x => (x._1, x._2, x._3.sum / x._3.length.toDouble, x._3))
+      .map(x => (x._1, x._2, x._4.map(y => Math.pow(y - x._3, 2) / x._2.toDouble).sum))
+    lazy val nhSh2 = rows.groupBy(x => (x(8), x(9), x(10)))
+      .map(x => (x._1, x._2.size, x._2.map(y => y(priceIndex).asInstanceOf[java.math.BigDecimal].doubleValue()).toList))
+      .map(x => (x._1, x._2, x._3.sum / x._3.length.toDouble, x._3))
+      .map(x => (x._1, x._2, x._4.map(y => Math.pow(y - x._3, 2) / x._2.toDouble).sum))
+    lazy val nhSh3 = rows.groupBy(x => (x(10), x(11), x(12), x(14)))
+      .map(x => (x._1, x._2.size, x._2.map(y => y(priceIndex).asInstanceOf[java.math.BigDecimal].doubleValue()).toList))
+      .map(x => (x._1, x._2, x._3.sum / x._3.length.toDouble, x._3))
+      .map(x => (x._1, x._2, x._4.map(y => Math.pow(y - x._3, 2) / x._2.toDouble).sum))
+    lazy val nhSh4 = rows.groupBy(x => (x(4), x(13), x(14)))
+      .map(x => (x._1, x._2.size, x._2.map(y => y(priceIndex).asInstanceOf[java.math.BigDecimal].doubleValue()).toList))
+      .map(x => (x._1, x._2, x._3.sum / x._3.length.toDouble, x._3))
+      .map(x => (x._1, x._2, x._4.map(y => Math.pow(y - x._3, 2) / x._2.toDouble).sum))
+    lazy val nhSh5 = rows.groupBy(x => (x(2), x(4)))
+      .map(x => (x._1, x._2.size, x._2.map(y => y(priceIndex).asInstanceOf[java.math.BigDecimal].doubleValue()).toList))
+      .map(x => (x._1, x._2, x._3.sum / x._3.length.toDouble, x._3))
+      .map(x => (x._1, x._2, x._4.map(y => Math.pow(y - x._3, 2) / x._2.toDouble).sum))
+    val nhShList = List(nhSh1, nhSh2, nhSh3, nhSh4, nhSh5)
+
+    def estimateSize(input: RDD[_]): Long = {
+      return SizeEstimator.estimate(input.collect())
+    }
+
+    def sampler(indexes: List[Int], nhShSampling: RDD[_], start: Double = 0.03, end: Double = 0.8, step: Double = 0.03): RDD[_] = {
+      var finalSample: (RDD[_], Double) = (null, 0)
+      var probability: Double = start
+      var sampleData: RDD[Row] = null
+      var nhSh2Sample: RDD[(Any, Int)] = null
+
+      while (finalSample._1 == null && probability < end) {
+        indexes.length match {
+          case 1 =>
+            val fractions = rows.map(x => x(indexes(0)))
+              .distinct()
+              .map(x => (x, probability))
+              .collectAsMap()
+            sampleData = rows.keyBy(x => x(indexes(0)))
+              .sampleByKeyExact(false, fractions)
+              .map(x => x._2)
+            nhSh2Sample = sampleData.groupBy(x => x(indexes(0)))
+              .map(x => (x._1, x._2.size))
+          case 2 =>
+            val fractions = rows.map(x => (x(indexes(0)), x(indexes(1))))
+              .distinct()
+              .map(x => (x, probability))
+              .collectAsMap()
+            sampleData = rows.keyBy(x => (x(indexes(0)), x(indexes(1))))
+              .sampleByKeyExact(false, fractions)
+              .map(x => x._2)
+            nhSh2Sample = sampleData.groupBy(x => (x(indexes(0)), x(indexes(1))))
+              .map(x => (x._1, x._2.size))
+          case 3 =>
+            val fractions = rows.map(x => (x(indexes(0)), x(indexes(1)), x(indexes(2))))
+              .distinct()
+              .map(x => (x, probability))
+              .collectAsMap()
+            sampleData = rows.keyBy(x => (x(indexes(0)), x(indexes(1)), x(indexes(2))))
+              .sampleByKeyExact(false, fractions)
+              .map(x => x._2)
+            nhSh2Sample = sampleData.groupBy(x => (x(indexes(0)), x(indexes(1)), x(indexes(2))))
+              .map(x => (x._1, x._2.size))
+          case 4 =>
+            val fractions = rows.map(x => (x(indexes(0)), x(indexes(1)), x(indexes(2)), x(indexes(3))))
+              .distinct()
+              .map(x => (x, probability))
+              .collectAsMap()
+            sampleData = rows.keyBy(x => (x(indexes(0)), x(indexes(1)), x(indexes(2)), x(indexes(3))))
+              .sampleByKeyExact(false, fractions)
+              .map(x => x._2)
+            nhSh2Sample = sampleData.groupBy(x => (x(indexes(0)), x(indexes(1)), x(indexes(2)), x(indexes(3))))
+              .map(x => (x._1, x._2.size))
+        }
+
+        // Get nh and sh
+        val mergedRdd = nhSh2Sample.keyBy(x => x._1)
+          .join(nhShSampling.asInstanceOf[RDD[(Any, Int, Double)]].keyBy(x => x._1))
+          .map(x => (x._2._1._2, x._2._2._2, x._2._2._3))
+
+        val nSample = n
+
+        // Calculate relative error
+        val errorValue = mergedRdd.map(x => (nSample / x._1) * Math.pow(x._2, 2) * x._3)
+          .collect()
+          .sum / nSample
+
+        // Calculate true error
+        val trueError = z * Math.pow(errorValue, 0.5)
+
+        val meanSample: List[Double] = sampleData.map(x => x(priceIndex))
+          .collect()
+          .map(x => x.asInstanceOf[java.math.BigDecimal].doubleValue())
+          .toList
+
+        val meanValue: Double = meanSample.foldLeft(0.0)(_ + _)
+
+        val totalError: Double = trueError / meanValue
+
+        // Check if value is inbounds
+        if (totalError <= e && totalError > 0) {
+          finalSample = (sampleData, totalError)
+        } else if (totalError == 0 && probability > 0.3) {
+          finalSample = (null, totalError)
+        } else {
+          finalSample = (null, totalError)
+        }
+
+        println("Probability")
+        println(probability)
+        println(finalSample._2)
+
+        probability = probability + step
+      }
+      finalSample._1
+    }
+
+    var returnVal = samplesList.zip(nhShList)
+      .map(x => (sampler(x._1, x._2, 0.15, 0.8, 0.1), x._1))
+      .map(x => (x._1, estimateSize(x._1), x._2))
+
+    var sizeTot: Long = 0
+    var i: Int = 0
+
+    while (i < returnVal.length) {
+      sizeTot = sizeTot + returnVal(i)._2
+
+      if (sizeTot > storageBudgetBytes) {
+        sizeTot = sizeTot - returnVal(i)._2
+        returnVal = returnVal.drop(i)
+      } else {
+        i = i + 1
+      }
+    }
+
+    (returnVal.map(x => x._1), returnVal.map(x => x._3))
   }
 }
